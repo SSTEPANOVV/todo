596eab044fbebc4b5be9b17e893ab709c970954f
Andrii Stepanov <andriystepanov2003@gmail.com>
2024-07-14 11:10:57 +0300

    Add the basic structure of the page

---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import { Body } from "./Body"
+import { Footer } from "./Footer"
+import { Header } from "./Header"
+
+export const Layout = () => {
+  return (
+    <>
+        <Header />
+        <Body />
+        <Footer />
+    </>
+  )
+}
