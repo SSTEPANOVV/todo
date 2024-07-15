b9c40653352fe5b500b084a3f0baad6a03b40bb2
Andrii Stepanov <andriystepanov2003@gmail.com>
2024-07-15 12:36:43 +0300

    Add Body section markup

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,5 +1,23 @@
+import checkImg from "../assets/check-circle.svg"
+import editImg from "../assets/edit-3.svg"
+import deleteImg from "../assets/trash-2.svg"
+
 export const Body = () => {
   return (
-    <div>Body</div>
+    <main className="main">
+        <div className="main_task">
+            <div className="main_wrapper">
+                <div className="main_info">
+                    <img src={checkImg} alt="" className="main_check" />
+                    <span className="main_text">Task #1</span>
+            </div>
+                <div className="main_buttons">
+                    <img src={editImg} alt="" className="main_edit" />
+                    <img src={deleteImg} alt="" className="main_delete" />
+            </div>
+            </div>
+            <p className="main_description">Task #1 Description:</p>
+        </div>
+    </main>
   )
 }
