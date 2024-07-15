d72fb32636eabe7b8831989b30dd6c5b622ece1f
Andrii Stepanov <andriystepanov2003@gmail.com>
2024-07-15 12:47:11 +0300

    Add Footer section markup

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,17 @@
+import alertImg from "../assets/alert-circle.svg"
+import shareImg from "../assets/share-2.svg"
+import formatImg from "../assets/format-icon.svg"
+import sortImg from "../assets/sort-icon.svg"
+import deleteImg from "../assets/trash-1.svg"
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+        <img src={alertImg} alt="" className="footer_button"/>
+        <img src={shareImg} alt="" className="footer_button"/>
+        <img src={formatImg} alt="" className="footer_button"/>
+        <img src={sortImg} alt="" className="footer_button"/>
+        <img src={deleteImg} alt="" className="footer_button"/>
+    </footer>
   )
 }
