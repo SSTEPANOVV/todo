4e64fa3c825301973fd3ea714dc60b68b39f820b
Andrii Stepanov <andriystepanov2003@gmail.com>
2024-07-15 14:05:18 +0300

    Add the functionality to add new task to local storage

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,21 +3,21 @@ import editImg from "../assets/edit-3.svg"
 import deleteImg from "../assets/trash-2.svg"
 
 export const Body = () => {
-  return (
-    <main className="main">
-        <div className="main_task">
-            <div className="main_wrapper">
-                <div className="main_info">
-                    <img src={checkImg} alt="" className="main_check" />
-                    <span className="main_text">Task #1</span>
+    return (
+        <main className="main">
+            <div className="main_task">
+                <div className="main_wrapper">
+                    <div className="main_info">
+                        <img src={checkImg} alt="" className="main_check" />
+                        <span className="main_text">Task #1</span>
+                    </div>
+                    <div className="main_buttons">
+                        <img src={editImg} alt="" className="main_edit" />
+                        <img src={deleteImg} alt="" className="main_delete" />
+                    </div>
+                </div>
+                <p className="main_description">Task #1 Description:</p>
             </div>
-                <div className="main_buttons">
-                    <img src={editImg} alt="" className="main_edit" />
-                    <img src={deleteImg} alt="" className="main_delete" />
-            </div>
-            </div>
-            <p className="main_description">Task #1 Description:</p>
-        </div>
-    </main>
-  )
+        </main>
+    )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,39 @@
+import { useState } from "react"
+
+interface Task {
+    id: number,
+    title: string,
+    description: string
+}
+
 export const Header = () => {
-  return (
-    <header className="header">
-        <h1 className="header__title">My Tasks</h1>
-        <form action="" className="header__form">
-            <input type="text" placeholder="Title"/>
-            <input type="text" placeholder="Description"/>
-            <button className="header_button">Add</button>
-        </form>
-    </header>
-  )
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    const addTask = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const newTask: Task = {
+            id: Date.now(),
+            title: title,
+            description: description
+        };
+
+        const storedTasks = localStorage.getItem("tasks");
+        const tasks: Task[] = storedTasks ? JSON.parse(storedTasks) : [];
+        tasks.push(newTask);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        setTitle("");
+        setDescription("");
+    }
+
+    return (
+        <header className="header">
+            <h1 className="header__title">My Tasks</h1>
+            <form onSubmit={addTask} className="header__form">
+                <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
+                <button className="header_button">Add</button>
+            </form>
+        </header>
+    )
 }
